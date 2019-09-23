d350bbf96ccdb5d22cdc1449c941a7a8ce963039
Gregory Tang <gtang159@gmail.com>
2019-09-23 16:53:40 -0700

    Created initial even t handlers for mouseover and addToCart event as well as created baseline UI

---
+++ shopping-cart-app/src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="app">
+    <div class="product">
+      <div class="product-img">
+        <img class="sock" :src="image" />
+      </div>
+      <div class="product-info">
+        <h1>{{ product }}</h1>
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+
+        <ul>
+          <li v-for="detail in details" :key="detail.id">{{ detail }}</li>
+        </ul>
+
+        <div v-for="variant in variants" :key="variant.variantID">
+          <p @mouseover="updateProduct(variant.variantImage)">{{ variant.variantColor }}</p>
+        </div>
+        <button v-on:click="addToCart">Add to Cart</button>
+        <div class="cart">
+          <p>Cart: {{ cart }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      product: "Socks",
+      image:
+        "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
+      inStock: true,
+      details: ["80% cotton", "20% polyester", "Gender-neutral"],
+      variants: [
+        {
+          variantID: 2234,
+          variantColor: "green",
+          variantImage:
+            "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg"
+        },
+        {
+          variantID: 2235,
+          variantColor: "blue",
+          variantImage:
+            "https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg"
+        }
+      ],
+      cart: 0
+    };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage;
+    }
+  }
+};
+</script>
+
+<style>
+body {
+  font-family: tahoma;
+  color: #282828;
+  margin: 0px;
+}
+.product {
+  display: flex;
+  flex-direction: row;
+}
+.product-img {
+  width: 50%;
+}
+.product-info {
+  margin-top: 10px;
+}
+.sock {
+  border: 1px solid #d8d8d8;
+  width: 70%;
+  margin: 40px;
+  box-shadow: 0px 0.5px 1px;
+}
+</style>
